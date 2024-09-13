72736f410535b299376a268460f416e466149b08
Juan Carlos <gurquiza8@gmail.com>
2024-09-13 15:00:05 -0600

    feature: validations using rexeg

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-// import * as customValidators from '../../../shared/validators/validators';
-
+import * as customValidators from '../../../shared/validators/validators';
 import { ValidatorsService } from '../../../shared/service/validators.service';
-import { EmailValidator } from '../../../shared/validators/email-validator.service';
-import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -12,10 +9,10 @@ import { cantBeStrider } from '../../../shared/validators/validators';
 export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [ Validators.required ]],
+    name: ['', [ Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern) ]],
     // email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ new EmailValidator() ]],
-    email: ['', [ Validators.required ]],
-    username: ['', [ Validators.required, cantBeStrider ]],
+    email: ['', [ Validators.required, Validators.pattern(customValidators.emailPattern) ]],
+    username: ['', [ Validators.required, customValidators.cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     password2: ['', [ Validators.required ]]
   });
@@ -24,7 +21,6 @@ export class RegisterPageComponent {
   constructor(
     private fb: FormBuilder,
     private validatorsService: ValidatorsService,
-    private emailValidator: EmailValidator
   ) {}
 
   isValidField( field: string ) {
